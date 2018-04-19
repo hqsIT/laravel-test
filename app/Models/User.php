e291ab608a0e999e28f593f8bc26a2b8a4146b7e
klinson <klinson@163.com>
2018-04-19 16:18:55 +0800

    finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function gravatar($size)
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
