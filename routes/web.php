8c3753c85de6d2e2f722d008b6901f540a641189
klinson <klinson@163.com>
2018-04-24 10:44:47 +0800

    finish user activate

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/test', 'TestController@index');
 
-Route::get('/signup', 'UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
